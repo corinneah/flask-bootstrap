03102f3d1ad41ebb814ee8e7f177a1b57b968a92
corinneah <103472876+corinneah@users.noreply.github.com>
2022-09-18 23:14:37 -0400

    update

--- app2.py
+++ app2.py
@@ -1,1 +1,18 @@
+# import packages 
+from flask import Flask, render_template
+import requests 
 
+app = Flask(__name__)
+
+# first page
+@app.route('/')
+def home_page():
+    return render_template('homepage.html')
+
+# second page
+@app.route('/other')
+def other_page():
+    return render_template('other.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
